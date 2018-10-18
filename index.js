a7aeb0b7e6c7324f309d0ba494554795f6e71831
vinnyvince34 <vseliang@gmail.com>
2018-10-18 10:06:25 +0700

    Next next commit

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
-    res.sendfile('app.html');
+    res.sendfile('index.html');
 });
 
 io.on('connection', function (socket) {
